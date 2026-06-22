bf990a147270d7382aea65bb9b5a6d493077b397
kenc9123 <kenc912333@gmail.com>
2026-06-22 23:39:43 +0800

    react

--- commit.bat
+++ commit.bat
@@ -4,6 +4,7 @@ git init
 git add .
 set /p msg=Explanation: 
 git commit -m "%msg%"
-git push
-echo Pushing!
+git remote add origin https://github.com/kenc9123/Web3_Learning
+git push -u origin main
+echo Pushed!
 pause
